cc70ca54d5899fc14d9f03de431cbce987948f7f
Realer18 <vutiendatt14052006@gmail.com>
2026-04-02 15:33:23 +0700

    Update 5.sql

--- Truy vấn/5.sql
+++ Truy vấn/5.sql
@@ -1,4 +1,4 @@
----Lấy thông tin căn hộ + quản lý
-SELECT ch.SoCanHo, ch.SoTang, nql.TenNguoiQuanLy
-FROM CanHo ch
-JOIN NguoiQuanLy nql ON ch.IDNguoiQuanLy = nql.IDNguoiQuanLy;
+---Lấy danh sách hóa đơn chưa thanh toán
+SELECT *
+FROM HoaDonCuDan
+WHERE TrangThai = N'Chưa thanh toán';
