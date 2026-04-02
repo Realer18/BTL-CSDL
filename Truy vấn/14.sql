9386a9514a8dbf0b735b6c2427ce9c5fefff140d
Realer18 <vutiendatt14052006@gmail.com>
2026-04-02 15:37:07 +0700

    Update 14.sql

--- Truy vấn/14.sql
+++ Truy vấn/14.sql
@@ -1,5 +1,5 @@
----Tổng số tiền hóa đơn theo quản lý
-SELECT nql.TenNguoiQuanLy, SUM(hd.SoTien) AS TongTien
-FROM HoaDonCuDan hd
-JOIN NguoiQuanLy nql ON hd.IDNguoiQuanLy = nql.IDNguoiQuanLy
-GROUP BY nql.TenNguoiQuanLy;
+---Truy vấn JOIN nhiều bảng (full thông tin cư dân + căn hộ + quản lý)
+SELECT CD.TenCuDan, CH.SoCanHo, NQL.TenNguoiQuanLy
+FROM CuDan CD
+JOIN CanHo CH ON CD.SoCanHo = CH.SoCanHo
+JOIN NguoiQuanLy NQL ON CH.IDNguoiQuanLy = NQL.IDNguoiQuanLy;
