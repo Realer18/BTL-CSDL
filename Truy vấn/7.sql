d8541998d429dc17bc5175eed0a869d6313b78ae
Realer18 <vutiendatt14052006@gmail.com>
2026-04-02 15:34:13 +0700

    Update 7.sql

--- Truy vấn/7.sql
+++ Truy vấn/7.sql
@@ -1,9 +1,5 @@
----Lấy tất cả phương tiện của 1 cư dân
-SELECT cd.TenCuDan, 'XeMay' AS Loai, xm.BienSoXe
-FROM XeMay xm JOIN CuDan cd ON xm.IDCuDan = cd.IDCuDan
-UNION
-SELECT cd.TenCuDan, 'XeOTo', xo.BienSoXe
-FROM XeOTo xo JOIN CuDan cd ON xo.IDCuDan = cd.IDCuDan
-UNION
-SELECT cd.TenCuDan, 'XeDap', xd.LoaiXe
-FROM XeDap xd JOIN CuDan cd ON xd.IDCuDan = cd.IDCuDan;
+---Danh sách cư dân có xe máy
+SELECT CD.TenCuDan, XM.BienSoXe
+FROM XeMay XM
+JOIN CuDan CD 
+ON XM.IDCuDan = CD.IDCuDan;
