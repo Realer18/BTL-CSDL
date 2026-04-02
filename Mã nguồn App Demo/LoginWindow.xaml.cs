2b8e42e66719920b0bd233b86b7dee008012547a
duc972 <huyduc3103@gmail.com>
2026-04-02 22:45:11 +0700

    Add files via upload

---
+++ Mã nguồn App Demo/LoginWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using QuanLyChungCu.ConnectDatabase;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuanLyChungCu
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window {
+        private AppDbContext dbContext = new AppDbContext();
+        private AuthService authService;
+
+        public LoginWindow() {
+            InitializeComponent();
+            authService = new AuthService(dbContext);
+        }
+
+        private void Window_Closed(object sender, EventArgs e) {
+            Application.Current.Shutdown();
+        }
+        public void btnLogin_Click(object sender, RoutedEventArgs e) {
+            if (!AllowLogin()) {
+                return;
+            }
+            var user = authService.Login(txtUserName.Text, txtPassword.Password);
+
+            if (user != null) {
+                if(txtUserName.Text.Trim() == user.IDTaiKhoan) {
+                    if (txtPassword.Password.Trim() == user.MatKhau) {
+                        Application.Current.Properties["UserRole"] = user.QuyenHan;
+                        Application.Current.Properties["ID"] = user.IDTaiKhoan;
+                        Application.Current.Properties["MK"] = user.MatKhau;
+                        if (user.QuyenHan == "Cư dân") {
+                            // Lấy thông tin căn hộ của cư dân từ bảng CuDan
+                            var cuDan = dbContext.CuDan
+                                                 .Where(c => c.IDCuDan == user.IDTaiKhoan)  // Lấy cư dân theo IDCudan
+                                                 .FirstOrDefault();
+
+                            if (cuDan != null) {
+                                Application.Current.Properties["SoCanHo"] = cuDan.SoCanHo;
+                            }
+                        }
+                        MainWindow maindWin = new MainWindow();
+                        Application.Current.MainWindow = maindWin;
+                        maindWin.Show();
+                        this.Hide();
+
+                        maindWin.Logout += mainWindow_Logout;
+                        return;
+                    }
+                    else {
+                        MessageBox.Show("Mật khẩu bạn nhập không chính xác", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                } else {
+                    MessageBox.Show("Tên đăng nhập không chính xác", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+                }                
+            } else {
+                MessageBox.Show("Tài khoản bạn nhập không chính xác", "Lỗi", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    private void mainWindow_Logout(object sender, EventArgs e) {
+        (sender as MainWindow).isExit = false;
+        this.Show();  // Hiển thị lại cửa sổ đăng nhập
+        (sender as MainWindow).Close();  // Đóng cửa sổ MainWindow
+    }
+    private bool AllowLogin() {
+        if (txtUserName.Text.Trim() == "") {
+            MessageBox.Show("Bạn chưa nhập Tài khoản", "Cảnh báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+            txtUserName.Focus();
+            return false; // Nếu chưa nhập thông tin thì thoát tại đây
+        }
+        if (txtPassword.Password.Trim() == "") {
+            MessageBox.Show("Bạn chưa nhập Mật khẩu", "Cảnh báo", MessageBoxButton.OK, MessageBoxImage.Warning);
+            txtPassword.Focus();  // Sửa lại là txtPassword.Focus() thay vì txtUserName.Focus()
+            return false; // Nếu chưa nhập thông tin thì thoát tại đây
+        }
+        return true;
+    }
+}
+}
---
+++ Mã nguồn App Demo/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using QuanLyChungCu.CustomControl;
+using QuanLyChungCu.Pages;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLyChungCu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool isExit = true;
+        public event EventHandler Logout;
+        public static MainWindow Instance { get; private set; }
+        public MainWindow() {
+            InitializeComponent();
+
+            MainWindowFrame.Navigate(new Dashboard());
+            Instance = this;
+
+        }
+        private void btnLogout_Click(object sender, RoutedEventArgs e) {
+            // Gọi sự kiện Logout khi người dùng bấm nút Logout
+            Logout?.Invoke(this, EventArgs.Empty);
+        }
+
+        //private void Window_Closed(object sender, EventArgs e) {
+        //    if (isExit && Application.Current.Windows.Count == 1)
+        //        Application.Current.Shutdown();
+        //}
+
+        //private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+        //    if (isExit) {
+        //        if (MessageBox.Show("Bạn muốn thoát chương trình", "Cảnh báo", MessageBoxButton.YesNo) != MessageBoxResult.Yes) {
+        //            e.Cancel = true;
+        //        }
+        //    }
+        //}
+    }
+}
