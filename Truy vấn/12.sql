5be9d6b88c708ee2a4ace0fc6d1e1bd47c33be3b
Realer18 <vutiendatt14052006@gmail.com>
2026-04-02 15:35:58 +0700

    Update 12.sql

--- Truy vấn/12.sql
+++ Truy vấn/12.sql
@@ -1,5 +1,10 @@
----Tìm quản lý phụ trách nhiều căn hộ nhất
-SELECT TOP 1 IDNguoiQuanLy, COUNT(*) AS SoCanHoQuanLy
-FROM CanHo
-GROUP BY IDNguoiQuanLy
-ORDER BY SoCanHoQuanLy DESC;
+---Thống kê số lượng xe của mỗi cư dân
+SELECT CD.TenCuDan,
+       COUNT(DISTINCT XM.IDXeMay) AS SoXeMay,
+       COUNT(DISTINCT XO.IDXeOTo) AS SoXeOTo,
+       COUNT(DISTINCT XD.IDXeDap) AS SoXeDap
+FROM CuDan CD
+LEFT JOIN XeMay XM ON CD.IDCuDan = XM.IDCuDan
+LEFT JOIN XeOTo XO ON CD.IDCuDan = XO.IDCuDan
+LEFT JOIN XeDap XD ON CD.IDCuDan = XD.IDCuDan
+GROUP BY CD.TenCuDan;
