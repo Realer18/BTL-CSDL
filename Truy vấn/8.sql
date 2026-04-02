46c548f2abefa74d670a696ab89e565fdede9d97
Realer18 <vutiendatt14052006@gmail.com>
2026-04-02 15:34:48 +0700

    Update 8.sql

--- Truy vấn/8.sql
+++ Truy vấn/8.sql
@@ -1,4 +1,3 @@
----Danh sách mặt bằng thương mại + hóa đơn
-SELECT mb.TenDonViThue, mb.GiaThue, hd.SoTien, hd.TrangThai
-FROM MatBangThuongMai mb
-JOIN HoaDonTM hd ON mb.IDMBTM = hd.IDMBTM;
+---Lấy thông tin tài khoản và quyền hạn
+SELECT IDTaiKhoan, QuyenHan
+FROM TaiKhoan;
